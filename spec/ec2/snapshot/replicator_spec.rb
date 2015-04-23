792435e3644745c85ba3b2f03255c954139bfcf5
Ryota Arai <ryota.arai@gmail.com>
2015-04-23 10:43:26 +0900

    Delete a spec generated by bundler.

--- spec/ec2/snapshot/replicator_spec.rb
+++ spec/ec2/snapshot/replicator_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Ec2::Snapshot::Replicator do
+describe EC2::Snapshot::Replicator do
   it 'has a version number' do
-    expect(Ec2::Snapshot::Replicator::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(EC2::Snapshot::Replicator::VERSION).not_to be nil
   end
 end
